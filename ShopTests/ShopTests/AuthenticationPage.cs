78c347a4bc367dd2211e156fd91ba0f2f586d1f2
SerhiySubbotinI <91838244+SerhiySubbotinI@users.noreply.github.com>
2021-11-25 13:48:13 +0200

    ShopTests
    C#

---
+++ ShopTests/ShopTests/AuthenticationPage.cs
@@ -0,0 +1,14 @@
+﻿using OpenQA.Selenium;
+
+namespace ShopTests
+{
+    internal class AuthenticationPage
+    {
+        private IWebDriver driver;
+
+        public AuthenticationPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+    }
+}
